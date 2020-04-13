74bbf650e82585d4fec31ed185fd834744b2ba48
Victor Montala <victormontala@gmail.com>
2020-04-13 18:09:04 +0200

    Adds linter, svg custom loader and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'extends': [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'rules': {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'js': 'never',
+        'jsx': 'never',
+        'ts': 'never',
+        'tsx': 'never',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        'extensions': [ '.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  },
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': [ '.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
