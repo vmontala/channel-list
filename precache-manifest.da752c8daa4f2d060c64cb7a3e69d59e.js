634a6c3c6f6c54cb47fc5dabe7bb1e082223601a
Victor Montala <victormontala@gmail.com>
2020-04-19 15:01:27 +0200

    Updates

---
+++ precache-manifest.da752c8daa4f2d060c64cb7a3e69d59e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbdebde3c339dd35e004af8714eb910",
+    "url": "/channel-list/index.html"
+  },
+  {
+    "revision": "9ea0b4586d4a29e30546",
+    "url": "/channel-list/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "61a08d00355ec3d39519",
+    "url": "/channel-list/static/css/main.451b3d35.chunk.css"
+  },
+  {
+    "revision": "9ea0b4586d4a29e30546",
+    "url": "/channel-list/static/js/2.395e6095.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/channel-list/static/js/2.395e6095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a08d00355ec3d39519",
+    "url": "/channel-list/static/js/main.e120a020.chunk.js"
+  },
+  {
+    "revision": "bd4b05202d3eb6c7ad89",
+    "url": "/channel-list/static/js/runtime-main.0020983e.js"
+  }
+]);
